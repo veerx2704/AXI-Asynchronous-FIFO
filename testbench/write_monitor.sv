7ddce096a6648e174ed2ce46959a0e5c22059f29
veerx2704 <veer.mukadam7272@gmail.com>
2026-08-31 00:21:14 +0530

    UVM WRITE MONITOR

--- testbench/write_monitor.sv
+++ testbench/write_monitor.sv
@@ -35,22 +35,68 @@ class write_monitor extends uvm_monitor#(transaction);
         forever begin
             if (v_wintf.wrst == 1) begin
                 trans = transaction::type_id::create("trans",this);
-            end
-        end
-        fork begin
-            begin
-                @(posedge v_wintf.s_axi_wclk)
-                while (v_wintf.awvalid == 0 || v_wintf.awready == 0) begin
-                    @(posedge v_wintf.s_axi_wclk);
-                end
-                trans.awvalid   =   `vif.awvalid;
-                trans.awready   =   `vif.awready;
-                trans.awlen     =   `vif.awlen;
-                trans.awaddr    =   `vif.awaddr;
-                trans.awaddr[0] =   new[trans.awlen+1];
+                fork begin
+                    begin : WRITE_ADDRESS_CHANNEL
+                        @(posedge v_wintf.s_axi_wclk)
+                        while (v_wintf.awvalid == 0 || v_wintf.awready == 0) begin
+                            @(posedge v_wintf.s_axi_wclk);
+                        end
+                        trans.awvalid   =   `vif.awvalid;
+                        trans.awready   =   `vif.awready;
+                        trans.awlen     =   `vif.awlen;
+                        trans.awaddr[0] =   `vif.awaddr;
+                        trans.awaddr    =   new[trans.awlen+1];
+                        trans.wdata     =   new[trans.awlen+1];
+                        sema.put(1);
+                    end : WRITE_ADDRESS_CHANNEL
 
+                    begin : WRITE_DATA_CHANNEL
+                        @(posedge v_wintf.s_axi_wclk);
+                        wdata_count = 0;
+                        repeat(v_wintf.wdata.size()) begin
+                            while(v_wintf.wvalid == 0 || v_wintf.wready == 0) begin
+                                @(posedge v_wintf.s_axi_wclk);
+                            end
+                            trans.wstrb = `vif.wstrb;
+                            trans.wvalid = `vif.wvalid;
+                            trans.wreadt = `vif.wvalid;
+                            trans.wdata[wdata_count] = `vif.wdata;
+                            wdata_count++;
+                            @(posedge v_wintf.s_axi_wclk);
+                        end
+                        sema.put(1);
+                    end : WRITE_DATA_CHANNEL
+
+                    begin : WRITE_RESPONSE_CHANNEL
+                        while (v_wintf.bvalid == 0 || v_wintf.bready == 0) begin
+                            @(posedge v_wintf.s_axi_wclk);
+                        end
+                        trans.bresp = `vif.bresp;
+                        trans.bready = `vif.bready;
+                        trans.bvalid = `vif.bvalid;
+                        sema.put(1);
+                    end : WRITE_RESPONSE_CHANNEL
+
+                    begin : MONITOR_WRITE_SCOREBOARD
+                        sema.get(3);
+                        monw2scor.write(trans);
+                        `uvm_info("MONITOR PACKETS SENT",$sformatf("%0s", trans.sprint),UVM_HIGH);
+                        `uvm_info("DATA CHECK: ",$sformatf("\n\n wdata == %p \n wsize == %0d",trans.wdata,trans.wdata.size),UVM_NONE);
+                    end : MONITOR_WRITE_SCOREBOARD
+                end
+                join_none
+                wait fork;
+            end
+            else begin
+                @(posedge v_wintf.s_axi_wclk);
+                trans = transaction::type_id::create("trans",this);
+                trans.wrst = `vif.wrst;
+                monw2scor.write(trans);
             end
         end
+    endtask
+
+    
 
 endclass
 
