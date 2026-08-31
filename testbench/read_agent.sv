a487ccfe7d813a95a2733c12f23563f6738677ea
veerx2704 <veer.mukadam7272@gmail.com>
2026-08-31 16:27:54 +0530

    UVM READ AGENT

---
+++ testbench/read_agent.sv
@@ -0,0 +1,31 @@
+`ifndef AXI_READ_AGENT
+`define AXI_READ_AGENT
+
+class read_agent extends uvm_agent;
+    `uvm_component_utils(read_agent);
+
+    sequencer sequencer_h;
+    read_driver driver_h;
+    read_monitor monitor_h;
+
+    uvm_analysis_port #(transaction) monr2scor;
+
+    function new(string name = "read_agent", uvm_component parent = null);
+        super.new(name,parent);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        sequencer_h = sequencer::type_id::create("sequencer_h",this);
+        driver_h = read_driver::type_id::create("driver_h", this);
+        monitor_h = read_monitor::type_id::create("monitor_h",this);
+    endfunction
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        driver_h.seq_item_port.connect(sequencer_h.seq_item_export);
+        monitor_h.monr2scor.connect(monr2scor);
+    endfunction
+
+endclass
+`endif
